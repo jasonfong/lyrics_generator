6ce82cefa870f3d76ea0e6807c72154f84eec18d
Jason Fong <jason.g.fong@gmail.com>
2019-09-15 17:30:07 -0700

    replace line type with line number; update docs for windows

--- README.md
+++ README.md
@@ -4,19 +4,31 @@ Song Lyrics Generator
 
 # Local Testing
 
-Install Python virtual environment:
+**Assuming Python 3 is already installed.**
+
+
+## Install Python virtual environment:
+
+**Mac / Linux:**
 ```
 python3 -m venv venv 
 source venv/bin/activate
 pip install -U -r dev-requirements.txt
 ```
 
-Run local Flask server:
+**Windows:**
+```
+python -m venv venv 
+./venv/Scripts/activate
+pip install -U -r dev-requirements.txt
+```
+
+## Run local Flask server:
 ```
 python main.py
 ```
 
-Browse to test form:
+## Browse to test form:
 http://localhost:8080/web/submit_line
 
 # Deploying to Google App Engine
--- models/line.py
+++ models/line.py
@@ -8,13 +8,15 @@ class Line:
     line_type_choices = ['any', 'chorus', 'verse', 'bridge']
     social_type_choices = ['instagram', 'facebook', 'twitter']
 
-    def __init__(self, text=None, line_type=None, social_type=None, social_id=None):
+    def __init__(self, text=None, line_number=None, social_type=None,
+                 social_id=None, approved=False):
         self.db = firestore.Client()
         self.id = str(uuid.uuid4())
         self.text = text
-        self.line_type = line_type
+        self.line_number = line_number
         self.social_type = social_type
         self.social_id = social_id
+        self.approved = approved
 
     
     @classmethod
@@ -25,13 +27,22 @@ class Line:
             for item in db.collection(cls.collection_name).stream()
         ]
 
+    
+    @classmethod
+    def get_unapproved(cls):
+        db = firestore.Client()
+        return [
+            item.to_dict()
+            for item in db.collection(cls.collection_name).where('approved', '==', False).stream()
+        ]
+
 
     def save(self):
         doc_ref = self.db.collection(self.collection_name).document(self.id)
         doc_ref.set({
             'id': self.id,
             'text': self.text,
-            'line_type': self.line_type,
+            'line_number': self.line_number,
             'social_type': self.social_type,
             'social_id': self.social_id,
         })
